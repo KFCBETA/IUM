f5d8075ce783439a424a76750620bac63e39bfcf
Heiru Wu <wuheiru.5203@gmail.com>
2019-05-21 19:04:09 -0400

    update models

--- django/backend/prof/models.py
+++ django/backend/prof/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length = 50)
+    user_email = models.EmailField()
+
+    def __str__(self):
+        return "User name: %s, email: %s" % (self.user_name, self.user_email)
+
+
+class Account(models.Model):
+    account_id = models.CharField(max_length = 100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # TODO: what else
+
+    def __str__(self):
+        return self.account_id
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.object.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
