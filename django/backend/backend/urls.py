50dcb2cad87c8d916ce2b061b7fef63f7dab04d1
Heiru Wu <heiru52310.usc@gmail.com>
2019-05-29 21:05:53 -0400

    update url and token settings

--- django/backend/backend/urls.py
+++ django/backend/backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', include('prof.urls', namespace='prof')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
